aa734494aa856e1afd3ea38b7b3ad8564d0c658f
심상갑 <kappa2410@gmail.com>
2022-09-30 10:51:31 +0900

    # merge

--- ToyProject/CoreData/Weather+CoreDataProperties.swift
+++ ToyProject/CoreData/Weather+CoreDataProperties.swift
@@ -1,25 +1,81 @@
 //
-//  Weather.swift
-//  ToyProject
+//  LocalCoordinate+CoreDataProperties.swift
 //
-//  Created by yeoboya on 2022/09/20.
+//
+//  Created by yeoboya on 2022/09/21.
+//
 //
 
 import Foundation
 import CoreData
 
-extension Weather {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<Weather> {
-        return NSFetchRequest<Weather>(entityName: "Weather")
+extension LocalCoordinate {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<LocalCoordinate> {
+        return NSFetchRequest<LocalCoordinate>(entityName: "LocalCoordinate")
     }
 
-    @NSManaged public var date: Date?
-    @NSManaged public var t1h: String?
-    @NSManaged public var sky: String?
-    @NSManaged public var reh: String?
-    @NSManaged public var pty: String?
-    @NSManaged public var rn1: String?
-    @NSManaged public var localCoordinate: LocalCoordinate?
+    @NSManaged public var coordX: Int16
+    @NSManaged public var coordY: Int16
+    @NSManaged public var latitude: Double
+    @NSManaged public var level1: String?
+    @NSManaged public var level2: String?
+    @NSManaged public var level3: String?
+    @NSManaged public var longitude: Double
+    @NSManaged public var weatherInfo: NSOrderedSet?
+
+    var localFullString: String {
+        guard let level1 = level1,
+              let level2 = level2,
+              let level3 = level3 else {
+            return ""
+        }
+
+        return "\(level1) \(level2) \(level3)"
+    }
+    
+    func toString() -> String {
+        guard let level1 = level1,
+              let level2 = level2,
+              let level3 = level3 else {
+            return "NO DATA"
+        }
+        return  "1단계: \(level1), 2단계: \(level2), 3단계: \(level3)\n" +
+                "좌표: (\(coordX), \(coordY))\n" +
+                "위도, 경도: (\(latitude), \(longitude))\n"
+    }
+    
+    func getPredicate() -> NSPredicate? {
+        guard let level1 = level1 else { return nil }
+        if let level2 = level2 {
+            if let level3 = level3 {
+                return NSPredicate(format: "level1 == %@ && level2 == %@ && level3 == %@", level1, level2, level3)
+            } else {
+                return NSPredicate(format: "level1 == %@ && level2 == %@ && level3 != %@", level1, level2, "")
+            }
+        } else {
+            return NSPredicate(format: "level1 == %@ && level2 != %@", level1, "")
+        }
+    }
+
 
 }
+
+// MARK: Generated accessors for weatherInfo
+extension LocalCoordinate {
+
+    @objc(addWeatherInfoObject:)
+    @NSManaged public func addToWeatherInfo(_ value: Weather)
+
+    @objc(removeWeatherInfoObject:)
+    @NSManaged public func removeFromWeatherInfo(_ value: Weather)
+
+    @objc(addWeatherInfo:)
+    @NSManaged public func addToWeatherInfo(_ values: NSSet)
+
+    @objc(removeWeatherInfo:)
+    @NSManaged public func removeFromWeatherInfo(_ values: NSSet)
+
+}
+
